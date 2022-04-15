22f1005b82eebcfacfb985a7fe5dc896e2bcc746
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-04-15 18:26:02 -0300

    fix: timer input, view and buttons

--- app/src/main/java/br/edu/ifgoiano/kronnos/MainActivity.java
+++ app/src/main/java/br/edu/ifgoiano/kronnos/MainActivity.java
@@ -21,4 +21,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, CronometroActivity.class);
         startActivity(intent);
     }
+
+    public void onClickTimer(View view) {
+        Intent intent = new Intent(MainActivity.this, TimerActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/br/edu/ifgoiano/kronnos/TimerActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifgoiano.kronnos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
